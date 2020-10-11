e5d025cf2ab7d31d5cea7dae4a3d259282f83f25
Daniel Garcia-Briseno <daniel.garcia.briseno@appsolutelywonderful.com>
2020-10-11 10:27:03 -0700

    more doc comments

--- lib/src/utils/structured_data_parser.dart
+++ lib/src/utils/structured_data_parser.dart
@@ -5,7 +5,10 @@ import 'json_ld_parser.dart';
 import 'microdata_parser.dart';
 import 'rdfa_parser.dart';
 
+/// Functions for extracting structured data from a given HTML document
 class StructuredDataParser {
+  /// Extracts jsonld, microdata, and rdfa data objects from the given
+  /// document
   static List<StructuredData> extract(Document document) {
     List<StructuredData> jsonLd = JsonLdParser.extractJsonLd(document);
     List<StructuredData> microdata = MicrodataParser.extractMicrodata(document);

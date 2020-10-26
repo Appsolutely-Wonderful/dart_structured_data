00adaca6d2599a47c0b5b3ded83d768e0d17cd8b
Daniel Garcia-Briseno <daniel.garcia.briseno@appsolutelywonderful.com>
2020-10-25 22:25:28 -0700

    Expose Iso8601 parser

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.1.2
+
+- Expose parser that can convert Iso8601 duration strings to Duration objects
+
 # 0.1.1
 
 - Any JSON-LD graphs will automatically be parsed into a List<StructuredData> object
--- lib/structured_data.dart
+++ lib/structured_data.dart
@@ -6,3 +6,4 @@ library structured_data;
 export 'src/objects/structured_data.dart';
 export 'src/html/data_importer.dart';
 export 'src/utils/structured_data_parser.dart';
+export 'src/objects/iso_8601.dart';

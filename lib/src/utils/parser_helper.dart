56c9c99e8bf3ca81692865dffc56f06e1a246d5f
Daniel Garcia-Briseno <daniel.garcia.briseno@appsolutelywonderful.com>
2020-10-29 11:07:03 -0700

    Fix error in jsonld parser

--- lib/src/utils/parser_helper.dart
+++ lib/src/utils/parser_helper.dart
@@ -1,9 +1,15 @@
 class ParserHelper {
-  static String stripProperty(String schema) {
+  static String stripProperty(dynamic schema) {
     if (schema != null) {
-      String stripped = schema.replaceFirst("http://schema.org/", "");
-      stripped = stripped.replaceFirst("https://schema.org/", "");
-      return stripped;
+      if (schema is String) {
+        String stripped = schema.replaceFirst("http://schema.org/", "");
+        stripped = stripped.replaceFirst("https://schema.org/", "");
+        return stripped;
+      } else if (schema is List) {
+        assert(schema.length == 1,
+            "Expected a single schema type, got multiple: $schema");
+        return stripProperty(schema[0]);
+      }
     } else {
       return schema;
     }

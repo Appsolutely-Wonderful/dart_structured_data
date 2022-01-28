b417192a39c7eab9c1cb7bbe74a7ade65d3358a0
andrflor <andrieu.florian@mail.com>
2022-01-28 12:33:04 +0100

    Added parse from string

--- lib/src/utils/structured_data_parser.dart
+++ lib/src/utils/structured_data_parser.dart
@@ -1,4 +1,5 @@
 import 'package:html/dom.dart';
+import 'package:html/parser.dart' as parser;
 
 import '../objects/structured_data.dart';
 import 'json_ld_parser.dart';
@@ -15,4 +16,9 @@ class StructuredDataParser {
     List<StructuredData> rdfa = RdfaParser.extractRdfa(document);
     return jsonLd + microdata + rdfa;
   }
+
+  /// Extracts jsonld, microdata, and rdfa data objects from the given
+  /// string
+  static List<StructuredData> parse(String string) =>
+      extract(parser.parse(string));
 }
